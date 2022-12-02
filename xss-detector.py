96b06658e986916d90cf9434910dafb349227c43
ism8el <82142134+ism8el@users.noreply.github.com>
2022-12-03 00:21:45 +0100

    Update xss-detector.py

--- xss-detector.py
+++ xss-detector.py
@@ -3,14 +3,7 @@
 import requests
 import sys
 import re
-
-url = sys.argv[1]
-t = []
-o = []
-gg = []
-
-i = 0
-j = 0
+import ast
 
 class bcolors:
     HEADER = '\033[95m'
@@ -23,9 +16,31 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-print(bcolors.OKCYAN + "ParamÃ¨tres trouvÃ©s:" + bcolors.ENDC)
+try:
+	sys.argv[1]
+except IndexError:
+	print(bcolors.WARNING + "\nUsage: python3 xss-detector.py <url_to_test> (Ex: python3 xss-detector.py http://example.fr/)\n" + bcolors.ENDC)
+	exit()
 
-resp = requests.get(url)
+
+cookie = input("Enter cookies like this: {'PHPSESSID':'1841ed304c0911ed9609c'} (press ENTER for nothing): ")
+try:
+	cookie
+	cookie = ast.literal_eval(cookie)
+except SyntaxError:
+	cookie = {}
+
+url = sys.argv[1]
+t = []
+o = []
+gg = []
+
+i = 0
+j = 0
+
+print(bcolors.OKCYAN + "Parameters found:" + bcolors.ENDC)
+
+resp = requests.get(url, cookies=cookie, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 path = resp.url
 
 with open("content.txt", "w") as content:
@@ -52,33 +67,33 @@ with open("content.txt", "r") as content:
 					print(laver)
 
 
-print(bcolors.OKCYAN + "\nTest des paramÃ¨tres avec GET:" + bcolors.ENDC)
+print(bcolors.OKCYAN + "\nParameter test with GET:" + bcolors.ENDC)
 
 while i < len(t):
-	get = requests.get(url, params={t[int(i)]: '<h1>f11a2gfa4erg1</h1>'})
+	get = requests.get(url, params={t[int(i)]: '<h1>f11a2gfa4erg1</h1>'}, cookies=cookie, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 
 	if '<h1>f11a2gfa4erg1</h1>' in get.text:
 		if t[int(i)] is not None:
-			print(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(i)] + "' vulnÃ©rable avec la mÃ©thode GET" + bcolors.ENDC)
-			gg.append(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(i)] + "' vulnÃ©rable avec la mÃ©thode GET" + bcolors.ENDC)
+			print(bcolors.OKGREEN + "Vulnerable '" + t[int(i)] + "' parameters with method GET" + bcolors.ENDC)
+			gg.append(bcolors.OKGREEN + "Vulnerable '" + t[int(i)] + "' parameters with method GET" + bcolors.ENDC)
 	else:
 		if t[int(i)] is not None:
-			print(bcolors.FAIL + "ParamÃ¨tre '" + t[int(i)] + "' non vulnÃ©rable avec la mÃ©thode GET" + bcolors.ENDC)
+			print(bcolors.FAIL + "Vulnerable '" + t[int(i)] + "' parameters with method GET" + bcolors.ENDC)
 	i = i + 1
 
 
-print(bcolors.OKCYAN + "\nTest des paramÃ¨tres avec POST:" + bcolors.ENDC)
+print(bcolors.OKCYAN + "\nParameter test with POST:" + bcolors.ENDC)
 
 while j < len(t):
-	post = requests.post(url, data={t[int(j)]: '<h1>f11a2gfa4erg1</h1>'})
+	post = requests.post(url, data={t[int(j)]: '<h1>f11a2gfa4erg1</h1>'}, cookies=cookie, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 
 	if '<h1>f11a2gfa4erg1</h1>' in post.text:
 		if t[int(j)] is not None:
-			print(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(j)] + "' vulnÃ©rable avec la mÃ©thode POST" + bcolors.ENDC)
-			gg.append(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(j)] + "' vulnÃ©rable avec la mÃ©thode POST" + bcolors.ENDC)
+			print(bcolors.OKGREEN + "Vulnerable '" + t[int(j)] + "' parameters with method POST" + bcolors.ENDC)
+			gg.append(bcolors.OKGREEN + "Vulnerable '" + t[int(j)] + "' parameters with method POST" + bcolors.ENDC)
 	else:
 		if t[int(j)] is not None:
-			print(bcolors.FAIL + "ParamÃ¨tre '" + t[int(j)] + "' non vulnÃ©rable avec la mÃ©thode POST" + bcolors.ENDC)
+			print(bcolors.FAIL + "Vulnerable '" + t[int(j)] + "' parameters with method POST" + bcolors.ENDC)
 
 	j = j + 1
 
@@ -88,7 +103,7 @@ print(bcolors.OKCYAN + "\nAutres pages trouvÃ©es:" + bcolors.ENDC)
 with open("content.txt", "r") as content:
 	for line in content:
 		for word in line.split():
-			if ".png" in word or ".jpg" in word or ".svg" in word or ".jpeg" in word or ".ico" in word or ".css" in word or ".js" in word or ".zip" in word or ".pdf" in word or ".txt" in word or ".gif" in word:
+			if ".html" in word or ".7z" in word or "#" in word or ".png" in word or ".jpg" in word or ".svg" in word or ".jpeg" in word or ".ico" in word or ".css" in word or ".js" in word or ".zip" in word or ".pdf" in word or ".txt" in word or ".gif" in word or ".JPEG" in word or ".PNG" in word or ".JPG" in word:
 				pass
 			else:
 				if 'href="' in word:
@@ -161,12 +176,12 @@ while x < len(o):
 	else:
 		new_url = path + o[x]
 
-	resp = requests.get(new_url)
+	resp = requests.get(new_url, cookies=cookie, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 
 	with open("content.txt", "w") as content:
 		content.write(resp.text)
 
-	print(bcolors.OKCYAN + "\nParamÃ¨tres trouvÃ©s sur la page", o[x] + bcolors.ENDC)
+	print(bcolors.OKCYAN + "\nParameters found on", new_url + bcolors.ENDC)
 
 	with open("content.txt", "r") as content:
 		for line in content:
@@ -187,45 +202,48 @@ while x < len(o):
 						t.append(laver)
 						print(laver)
 
-	print(bcolors.OKCYAN + "\nTest des paramÃ¨tres avec GET sur la page:", o[x] + bcolors.ENDC)
+	print(bcolors.OKCYAN + "\nParameter test with GET on:", new_url + bcolors.ENDC)
 
 	while i < len(t):
-		get = requests.get(new_url, params={t[int(i)]: '<h1>f11a2gfa4erg1</h1>'})
+		get = requests.get(new_url, params={t[int(i)]: '<h1>f11a2gfa4erg1</h1>'}, cookies=cookie, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 
 		if '<h1>f11a2gfa4erg1</h1>' in get.text:
 			if t[int(i)] is not None:
-				print(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(i)] + "' vulnÃ©rable avec la mÃ©thode GET" + bcolors.ENDC)
-				gg.append(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(j)] + "' vulnÃ©rable avec la mÃ©thode POST sur la page " + o[x] + bcolors.ENDC)
+				print(bcolors.OKGREEN + "Vulnerable '" + t[int(i)] + "' parameters with method GET" + bcolors.ENDC)
+				gg.append(bcolors.OKGREEN + "Vulnerable '" + t[int(j)] + "' parameters with method POST on " + new_url + bcolors.ENDC)
 
 		else:
 			if t[int(i)] is not None:
-				print(bcolors.FAIL + "ParamÃ¨tre '" + t[int(i)] + "' non vulnÃ©rable avec la mÃ©thode GET" + bcolors.ENDC)
+				print(bcolors.FAIL + "Vulnerable '" + t[int(i)] + "' parameters with method GET" + bcolors.ENDC)
 
 		i = i + 1
 
 
-	print(bcolors.OKCYAN + "\nTest des paramÃ¨tres avec POST sur la page:", o[x] + bcolors.ENDC)
+	print(bcolors.OKCYAN + "\nParameter test with POST on: " + new_url + bcolors.ENDC)
 
 	while j < len(t):
-		post = requests.post(new_url, data={t[int(j)]: '<h1>f11a2gfa4erg1</h1>'})
+		post = requests.post(new_url, data={t[int(j)]: '<h1>f11a2gfa4erg1</h1>'}, cookies=cookie, headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'})
 
 		if '<h1>f11a2gfa4erg1</h1>' in post.text:
 			if t[int(j)] is not None:
-				print(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(j)] + "' vulnÃ©rable avec la mÃ©thode POST" + bcolors.ENDC)
-				gg.append(bcolors.OKGREEN + "ParamÃ¨tre '" + t[int(j)] + "' vulnÃ©rable avec la mÃ©thode POST sur la page " + o[x] + bcolors.ENDC)
+				print(bcolors.OKGREEN + "Vulnerable '" + t[int(j)] + "' parameters with method POST" + bcolors.ENDC)
+				gg.append(bcolors.OKGREEN + "Vulnerable '" + t[int(j)] + "' parameters with method POST on " + new_url + bcolors.ENDC)
 		else:
 			if t[int(j)] is not None:
-				print(bcolors.FAIL + "ParamÃ¨tre '" + t[int(j)] + "' non vulnÃ©rable avec la mÃ©thode POST" + bcolors.ENDC)
+				print(bcolors.FAIL + "Vunerable '" + t[int(j)] + "' parameters with method POST" + bcolors.ENDC)
 
 		j = j + 1
 	x = x + 1
 
 print("\n---------------------------------------------------------------------")
-print(bcolors.OKCYAN + "\nRESUME DES FAILLES XSS TROUVEES:" + bcolors.ENDC)
+print(bcolors.OKCYAN + "\nSummary of faults found:" + bcolors.ENDC)
 y=0
 while y < len(gg):
 	print(gg[y])
 	y = y + 1
+
+if y == 0:
+	print(bcolors.FAIL + "\nNo XSS flaws found :(" + bcolors.ENDC)
 
 
 
